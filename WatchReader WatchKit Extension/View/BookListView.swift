6bc422030b3157335a27fd88090fc3a3895c75d6
hanhqvu <hanhqvu@users.noreply.github.com>
2022-08-26 15:03:31 +0900

    refactor: show DetailView as another screen instead of modal

--- WatchReader WatchKit Extension/View/BookListView.swift
+++ WatchReader WatchKit Extension/View/BookListView.swift
@@ -15,25 +15,16 @@ struct BookListView: View {
             List {
                 if (!bookList.isEmpty) {
                     ForEach(0..<bookList.count, id: \.self) { index in
-                        BookItemView(title: bookList[index].title, key: bookList[index].imageKey)
-                            .swipeActions(edge: .leading) {
-                                Button(role: .none) {
-                                    showDetail.toggle()
-                                } label: {
-                                    Label("Read", systemImage: "ellipsis")
+                        NavigationLink(destination: DetailView(book: $bookList[index])) {
+                            BookItemView(title: bookList[index].title, key: bookList[index].imageKey)
+                                .swipeActions(edge: .trailing) {
+                                    Button(role: .destructive) {
+                                        bookList.remove(at: index)
+                                    } label: {
+                                        Label("Remove", systemImage: "trash")
+                                    }
                                 }
-                                .tint(.green)
-                            }
-                            .swipeActions(edge: .trailing) {
-                                Button(role: .destructive) {
-                                    bookList.remove(at: index)
-                                } label: {
-                                    Label("Remove", systemImage: "trash")
-                                }
-                            }
-                            .sheet(isPresented: $showDetail) {
-                                DetailView(book: $bookList[index], showDetail: $showDetail)
-                            }
+                        }
                     }
                     .listRowBackground(Color(red: 0.98, green: 0.929, blue: 0.804))
                 }
