603982fe42732978fe1d5a5aa1f05ba8f1f0ee58
hanhqvu <hanhqvu@users.noreply.github.com>
2023-08-04 11:30:04 +0700

    feat: add context for persisting books

--- WatchReader App/Screens/Search/SearchViewModel.swift
+++ WatchReader App/Screens/Search/SearchViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreData
 
 @MainActor
 final class SearchViewModel: ObservableObject {
@@ -13,10 +14,40 @@ final class SearchViewModel: ObservableObject {
     @Published var searchResult: [BookRes] = []
     @Published var isLoading: Bool = false
     @Published var bookList: [Book] = []
+    let viewContext = StorageProvider.shared.persistentContainer.viewContext
+    let searchContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    init() {
+        searchContext.parent = viewContext
+    }
+    
+    func addBook(_ bookToAdd: BookRes) {
+        let book = BookEntity(context: searchContext)
+        book.title = bookToAdd.title
+        book.status = "Reading"
+        book.imageKey = bookToAdd.imageKey
+        book.summary = bookToAdd.title
+        
+        do {
+            try searchContext.save()
+            print("Book added succesfully")
+        } catch {
+            print("Failed to add book: \(error)")
+        }
+    }
     
     func dismiss() {
         keyword = ""
         searchResult = []
+    }
+    
+    func complete() {
+        do {
+            try viewContext.save()
+            print("Books saved succesfully")
+        } catch {
+            print("Failed to save books")
+        }
     }
     
     func getSearchResults() async {
