ae4492ad1dd651aceaa45b88026ce55cef7899d8
hanhqvu <hanhqvu@users.noreply.github.com>
2023-08-05 12:41:55 +0700

    feat: book status ui

--- WatchReader App/Views/SearchItemView.swift
+++ WatchReader App/Views/SearchItemView.swift
@@ -7,49 +7,64 @@
 
 import SwiftUI
 
+enum ListStatus {
+    case added, pending, none
+}
+
 struct SearchItemView: View {
     @State var bookRes: BookRes
-    @Binding var bookList: [Book]
+    @State var listStatus: ListStatus
     
     var body: some View {
-            HStack {
-                AsyncImage(url: URL(string: "https://covers.openlibrary.org/b/olid/\(bookRes.imageKey ?? "")-S.jpg"))  { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                    } placeholder: {
-                        Color.secondary
-                    }
-                    .frame(width: 25, height: 50, alignment: .leading)
-                VStack {
-                    Text("\(bookRes.title)")
-                        .font(.custom("Baskerville", size: 14, relativeTo: .headline))
-                        .frame(alignment: .leading)
-                        .foregroundColor(.black)
-                    Text("\(bookRes.author?[0] ?? "")")
-                        .font(.custom("Baskerville", size: 10, relativeTo: .subheadline))
-                        .frame(alignment: .leading)
-                        .foregroundColor(.black)
-                }
-                
-//                if (!bookList.contains(where: { element in
-//                    if element.id == bookRes.id {
-//                        return true
-//                    } else {
-//                        return false
-//                    }
-//                })) {
-//                    Button(role: .none) {
-//                        bookList.append(Book(id: bookRes.id, title: bookRes.title, author: bookRes.author ?? [], imageKey: bookRes.imageKey ?? "", status: Status.reading))
-//                        } label: {
-//                            Image(systemName: "plus")
-//                        }
-//                    .padding()
-//                    .background(.blue)
-//                    .clipShape(RoundedRectangle(cornerRadius: 50))
-//                }
+        HStack {
+            AsyncImage(url: URL(string: "https://covers.openlibrary.org/b/olid/\(bookRes.imageKey ?? "")-S.jpg"))  { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                Color.secondary
+            }
+            .frame(width: 25, height: 50, alignment: .leading)
+            VStack {
+                Text("\(bookRes.title)")
+                    .font(.custom("Baskerville", size: 14, relativeTo: .headline))
+                    .frame(alignment: .leading)
+                    .foregroundColor(.black)
+                Text("\(bookRes.author?[0] ?? "")")
+                    .font(.custom("Baskerville", size: 10, relativeTo: .subheadline))
+                    .frame(alignment: .leading)
+                    .foregroundColor(.black)
+            }
+            
+            switch (listStatus) {
+            case .added:
+                Image(systemName: "tray.circle")
+                    .padding()
+                    .foregroundColor(.green)
+                    .imageScale(.large)
+            case .pending:
+                Image(systemName: "minus.circle")
+                    .padding()
+                    .foregroundColor(.red)
+                    .imageScale(.large)
+            case .none:
+                Image(systemName: "plus.circle")
+                    .padding()
+                    .foregroundColor(.blue)
+                    .imageScale(.large)
+            }
+            
         }
         .padding(1)
         .frame(maxWidth: .infinity)
+        .onTapGesture {
+            switch (listStatus) {
+            case .added: return
+            case .pending:
+                listStatus = .none
+            case .none:
+                listStatus = .pending
+            }
+        }
     }
 }
